f271c919bb8c11ab7702ab1124909a360c38a899
Takeru <ken.aoki.1986@gmail.com>
2024-10-20 20:15:23 +0900

    add reserve for recalc func on change start or finish times of working record

--- service/reply_to_postback_update_working_record_service.py
+++ service/reply_to_postback_update_working_record_service.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import datetime
+import common.constant as co
+import service.shared.user_info_service as ui_sv
 import service.shared.working_record_service as wr_sv
 import service.shared.line_tool_service as lt_sv
 import service.shared.datetime_calc_service as dc_sv
@@ -17,10 +20,34 @@ def main(operating_mode, userInfo, postbacked_data):
         postbacked_data['new_val'] if postbacked_data['postbackedDateType'] == 'not_date' 
         else postbacked_data['postbackedDateValue']
     )
-    if updated['count'] > 0:
-        workingRecord = updated['workingRecord']
-    else:
+    if updated['count'] <= 0:
         return lt_sv.get_a_text_send_message('更新対象レコードの更新に失敗しました。')
+    else:
+        workingRecord = updated['workingRecord']
+        # 変更する要素がstart_timeかfinish_timeの場合、再計算バッチを予約する
+        if postbacked_data['tar_el'] == 'start_time' or postbacked_data['tar_el'] == 'finish_time':
+            # working_recordの予約
+            reserve_recalc_wr_result = wr_sv.update_working_record(
+            workingRecord,
+            'standby_status', 
+            co.STANDBY_STATUS_WAITING_BATCH_PROCESS_RECALCULATE,
+            datetime.datetime.now(),
+            'update_working_record_from_postback'
+            )
+            if reserve_recalc_wr_result['count'] <= 0:
+                return lt_sv.get_a_text_send_message('workingRecordの再計算予約に失敗しました。')
+            else:
+                # user_infoの再計算予約
+                reserve_recalc_ui_result = ui_sv.update_user_info(
+                    userInfo,
+                    'standby_status', 
+                    co.STANDBY_STATUS_WAITING_BATCH_PROCESS_RECALCULATE,
+                    datetime.datetime.now(),
+                    'update_working_record_from_postback'
+                )
+                if reserve_recalc_ui_result['count'] <= 0:
+                    return lt_sv.get_a_text_send_message('userInfoの再計算予約に失敗しました。')
+                
     ## 以下、返信テキストを作成する
     return lt_sv.get_a_text_send_message(
         '[変更完了]\n'
