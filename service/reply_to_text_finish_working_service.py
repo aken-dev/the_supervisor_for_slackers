d73d167db40488de1c48131c04f770ace619fec2
Takeru <ken.aoki.1986@gmail.com>
2024-10-20 16:30:32 +0900

    bug fix

--- service/reply_to_text_finish_working_service.py
+++ service/reply_to_text_finish_working_service.py
@@ -8,7 +8,7 @@ def main(operating_mode, userInfo, recieved_text):
     pre_chk = wr_sv.get_a_working_record_by_status(
         userInfo, co.PROCESS_CATEGORY_RECORD_WORKING_HOURS, co.PROCESS_STATUS_ON_RECORDING
     )
-    if pre_chk['count'] < 0: return lt_sv.get_a_text_send_message('今、何も作業してない事になってるぞ。')
+    if pre_chk['count'] < 1: return lt_sv.get_a_text_send_message('今、何も作業してない事になってるぞ。')
     finished = wr_sv.finish_the_work(userInfo)
     if finished['count'] > 0:
         workingRecord = finished['workingRecord']
